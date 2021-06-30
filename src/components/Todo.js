d454ab4bb59056e470090fe9c82baf3b00a2793d
jholmes16 <jholmesarea@gmail.com>
2021-06-30 14:06:02 -0400

    Add Modal and Backdrop comp. functional buttons

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,30 @@
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
 function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function deleteHandler() {
+    setModalIsOpen(true);
+  }
+
+  function closeModalhandler() {
+    setModalIsOpen(false);
+  }
+
   return (
     <div className="card">
       <h2>{props.title}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
+      {modalIsOpen && (
+        <Modal onCancel={closeModalhandler} onConfirm={closeModalhandler} />
+      )}
+      {modalIsOpen && <Backdrop onCancel={closeModalhandler} />}
     </div>
   );
 }
